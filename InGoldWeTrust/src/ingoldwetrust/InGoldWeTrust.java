c79e77984be9f56a35fdc9eaa14908e190b0201c
Racco <raccotaco@gmail.com>
2016-04-14 19:08:28 -0500

    Added files via upload
    Gui so far

---
+++ InGoldWeTrust/src/ingoldwetrust/InGoldWeTrust.java
@@ -0,0 +1,10 @@
+package ingoldwetrust;
+
+public class InGoldWeTrust {
+
+    public static void main(String[] args) {
+        startUI gui = new startUI();
+        gui.setVisible(true);
+    }
+    
+}
